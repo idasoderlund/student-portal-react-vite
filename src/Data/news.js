357784148dd02725c806ab4354edc2300ccb1ab9
idasoderlund <soderlundida7711@gmail.com>
2025-06-03 20:58:25 +0200

    fixed wrong spellings in the news array

--- src/Data/news.js
+++ src/Data/news.js
@@ -1,27 +1,27 @@
 export const news = [
   {
     id: 1,
-    title: "New Course in React- launched the 1st of September!",
+    title: "New Course in React- Launching the 1st of September!",
     body: "We are happy to tell ypu that a new course in React will be available to applay for in September. We are launching the new course September 1st. Stay tuned!",
   },
   {
     id: 2,
-    title: "Stipendium worthy school!",
+    title: "Stipendium-worthy school!",
     body: "Our students in class BC3 won a stipendium at the local Economial Calculation Race! We are celebrating with Cake in the Yellow Classroom 15.00. All students are welcome!",
   },
   {
     id: 3,
     title: "Think GREEN - Plant based Menu.",
-    body: "To improve the work for a greener plantet, our scool will be participating in the Thing Green Month together with 4 other scools. From 21/7 - 25/7 the school restaurant will be offering only plant based lunch.",
+    body: "To improve the work for a greener plantet, our scool will be participating in the Think Green Month together with 4 other scools. From 21/7 - 25/7 the school restaurant will be offering only plant based lunch. Stay tuned to get the updated Menu!",
   },
   {
     id: 4,
     title: "Welcome to our new janitor  - Ben! ",
-    body: "You students, as well as us teachers will see a new happy smile walking down the hallways in school. We all welcome our new janitor Ben McHarrel! Ben has his office in the red hallway at departnemtn 4, next to the reception. If you need anything from the janitor, dont hesitate to visit Ben! He will gladly help you all!",
+    body: "You students, as well as us teatchers will see a new happy smile walking down the hallways in school. We all welcome our new janitor Ben McHarrel! Ben has his office in the red hallway at department 4, next to the reception. If you need anything from the janitor, dont hesitate to visit Ben! He will gladly help you all. Welcome Ben!",
   },
   {
     id: 5,
     title: "Yearly Cleaning day is round the corner!",
-    body: "As you all know we have a yearly cleaning day. We are happy to tell you that the 1st of july are now scheduled for cleaning day here in school. All students will get there cleaning tools from Janitor Ben in the morning in each classroom. All the lockers should be shining clean at 13.30, after that - we teatchers will offer everyone lunch at the baseball plan.",
+    body: "As you all know we have a yearly cleaning day. We are happy to tell you that the 1st of july are now scheduled for cleaning day here in school. All students will get there cleaning tools from Janitor Ben in the morning in each classroom. All the lockers should be shining clean at 13.30, after that - we teatchers will offer everyone lunch at the baseball plan. Burgers will be served and the weather will be warm!",
   },
 ];
