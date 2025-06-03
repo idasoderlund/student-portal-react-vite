a024bfd9505cc0b49e6fdfc60e176d6db2e7d649
idasoderlund <soderlundida7711@gmail.com>
2025-06-03 22:35:14 +0200

    added funtion to courses and used array method  .map

---
+++ src/Pages/Courses.jsx
@@ -0,0 +1,20 @@
+import { courses } from "../Data/courses";
+import CourseCard from "../Components/CourseCard";
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function Courses() {
+  return (
+    <>
+      <h2>Courses</h2>
+      <Row>
+        {courses.map((course) => (
+          <Col key={course.id} md={4} className="mb-4">
+            <CourseCard course={course} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
+export default Courses;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="text">
+      <h1>Welcome to Student Portal</h1>
+      <p>
+        On this site you are able to see courses, reed the news and to register
+        for courses if interested!
+      </p>
+    </div>
+  );
+}
+export default Home;
