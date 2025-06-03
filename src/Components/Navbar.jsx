b267c90d64c3f8eb82cd0c565fea6bbb379df20e
idasoderlund <soderlundida7711@gmail.com>
2025-06-03 21:23:11 +0200

    added function for navbar with link paths

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+function NavigationBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">StudentPortal</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/courses">
+              <Nav.Link>Courses</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/news">
+              <Nav.Link>News</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/register">
+              <Nav.Link>Register</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+export default NavigationBar;
