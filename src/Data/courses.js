f4c7e996eb893a06b9e99a72c4235dbc19c0e4f0
idasoderlund <soderlundida7711@gmail.com>
2025-06-03 20:36:01 +0200

    added an array with courses

---
+++ src/Data/courses.js
@@ -0,0 +1,72 @@
+export const courses = [
+  {
+    id: 1,
+    name: "Math 1A",
+    description: "Basics in Math.",
+    details:
+      "This is a course to gain basic knowledges in math. The course includes algebra, geometry and calculation.",
+  },
+  {
+    id: 2,
+    name: "Programming 1",
+    description: "Introduction for programming.",
+    details:
+      "Learn the basic knowledges in programming. The course includes C++ and C#. The student will choose what language to specify in.",
+  },
+  {
+    id: 3,
+    name: "English 5",
+    description: "More advanced deeping in the English vocabulary.",
+    details:
+      "Improve your knowledges in English. The course focus on fiction and factual texts.",
+  },
+  {
+    id: 4,
+    name: "Math 1B",
+    description: "More advanced deeping in math.",
+    details:
+      "Improve your knowledges in Math. The course focus on probability theory and statistics.",
+  },
+  {
+    id: 5,
+    name: "Math 1C",
+    description: "More advanced deeping in math.",
+    details:
+      "Improve your knowledges in math. The course focus on exponential form and base exponential form.",
+  },
+  {
+    id: 6,
+    name: " 5",
+    description: "More advanced deeping in the English vocabulay",
+    details:
+      "Improve your knowledges in English. The course focus on fiction and factual texts.",
+  },
+  {
+    id: 7,
+    name: "Buisiness Economics 1",
+    description: "Basic knowledges in Buisiness Economics.",
+    details:
+      "Learn the basic knowledges in buisiness economics. The course focus on organization, marketing and accounting.",
+  },
+  {
+    id: 8,
+    name: "Buisiness Economics 2",
+    description: "More advanced deeping in the Buisiness Economics",
+    details:
+      "Improve your knowledges in buisiness economics. The course includes financial managemant.",
+  },
+  {
+    id: 9,
+    name: "Religion 1B",
+    description: "More advanced deeping in the different Religions.",
+    details:
+      "Improve your knowledges in Religion. The course focus on differences and similarities in different religions.",
+  },
+  {
+    id: 10,
+    name: "Swedish 3",
+    description: "More advanced deeping in the Swedish vocabulary.",
+    details:
+      "Improve your knowledges in Swedish. The course focus on fiction and factual texts.",
+  },
+];
