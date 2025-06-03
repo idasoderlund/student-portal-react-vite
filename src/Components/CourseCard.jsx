c25609a29d7d75cded4dd182fc76d9627070cf0b
idasoderlund <soderlundida7711@gmail.com>
2025-06-03 22:40:08 +0200

    added coursecard function that fetches the array with props for card

---
+++ src/Components/CourseCard.jsx
@@ -0,0 +1,17 @@
+import { Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function CourseCrad({ course }) {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{course.name}</Card.Title>
+        <Card.Text>{course.description}</Card.Text>
+        <Link to={`/courses/${course.id}`}>
+          <Button variant="primary">Show more</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+}
+export default CourseCrad;
