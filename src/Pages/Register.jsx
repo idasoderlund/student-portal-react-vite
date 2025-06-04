7b0bcb08dae6adc0e8b4f8f43e78888f68f8a7d4
idasoderlund <soderlundida7711@gmail.com>
2025-06-04 12:47:27 +0200

    added registerform

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,15 +1,77 @@
-import { createContext, useState } from "react";
-export const RegisterContext = createContext();
+import { useState, useContext } from "react";
+import { TextField, Button, MenuItem } from "@mui/material";
+import { RegisterContext } from "../Contexts/RegisterContext";
+import { courses } from "../Data/courses";
+import { useNavigate } from "react-router-dom";
 
-export function RegisterProvider({ children }) {
-  const [registrations, setRegistrations] = useState([]);
+function Register() {
+  const { addRegistration } = useContext(RegisterContext);
+  const navigate = useNavigate();
 
-  const addRegistrations = (registration) => {
-    setRegistrations([...registrations, registration]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [courseId, setCourseId] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const registration = {
+      name,
+      email,
+      courseId,
+    };
+    addRegistration(registration);
+    alert("Registration is saved!");
+    navigate("/");
   };
+
   return (
-    <RegisterContext.Provider value={{ registrations, addRegistrations }}>
-      {children}
-    </RegisterContext.Provider>
+    <div>
+      <h2>Register yourself for a course</h2>
+      <form onSubmit={handleSubmit} style={{ maxWidth: 400, margin: "auto" }}>
+        <TextField
+          label="Name"
+          fullWidth
+          margin="normal"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          reguired
+        />
+
+        <TextField
+          label="Email"
+          type="email"
+          fullWidth
+          margin="normal"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          reguired
+        />
+
+        <TextField
+          select
+          label="CHoose course"
+          fullWidth
+          margin="normal"
+          value={courseId}
+          onChange={(e) => setCourseId(e.target.value)}
+          reguired
+        >
+          {courses.map((course) => (
+            <MenuItem key={course.id} value={course.id}>
+              {course.name}
+            </MenuItem>
+          ))}
+        </TextField>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          style={{ marginTop: 16 }}
+        >
+          Register
+        </Button>
+      </form>
+    </div>
   );
 }
+export default Register;
